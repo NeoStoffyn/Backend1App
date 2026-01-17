7c25b4062f4b39fdbc6f305f963bce19b791413d
NeoStoffyn <stoffyn.neo@yahoo.com>
2026-01-17 16:18:57 +0100

    feat: convert FieldMessage to record and add CampusDto

--- campus/src/main/java/be/neostoffyn/campus/error/FieldMessage.java
+++ campus/src/main/java/be/neostoffyn/campus/error/FieldMessage.java
@@ -1,19 +1,4 @@
 package be.neostoffyn.campus.error;
 
-public class FieldMessage {
-    private String field;
-    private String message;
-
-    public FieldMessage(String field, String message) {
-        this.field = field;
-        this.message = message;
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public record FieldMessage(String field, String message) {
 }
