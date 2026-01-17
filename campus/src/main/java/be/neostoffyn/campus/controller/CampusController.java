94f332eaf574445f0a0b17f8181bb15b9be3e7e6
NeoStoffyn <stoffyn.neo@yahoo.com>
2026-01-17 16:18:48 +0100

    feat: convert FieldMessage to record and add CampusDto

--- campus/src/main/java/be/neostoffyn/campus/controller/CampusController.java
+++ campus/src/main/java/be/neostoffyn/campus/controller/CampusController.java
@@ -1,5 +1,6 @@
 package be.neostoffyn.campus.controller;
 
+import be.neostoffyn.campus.dto.CampusDto;
 import be.neostoffyn.campus.error.FieldMessage;
 import be.neostoffyn.campus.exception.Campus.CampusAlreadyExistsException;
 import be.neostoffyn.campus.exception.Campus.CampusInvalidDataException;
@@ -24,24 +25,26 @@ public class CampusController {
 
     // GET:/campus
     @GetMapping
-    public ResponseEntity<List<Campus>> getAllCampuses() {
-        return ResponseEntity.ok(campusService.getAllCampuses());
+    public ResponseEntity<List<CampusDto>> getAllCampuses() {
+        return ResponseEntity.ok(campusService.getAllCampuses().stream()
+                .map(CampusDto::from)
+                .toList());
     }
 
     // GET: /campus/{name}
     @GetMapping("/{name}")
-    public ResponseEntity<Campus> getCampus(@PathVariable String name) {
+    public ResponseEntity<CampusDto> getCampus(@PathVariable String name) {
         Campus campus = campusService.getCampusByName(name);
-        return ResponseEntity.ok(campus);
+        return ResponseEntity.ok(CampusDto.from(campus));
     }
 
 
 
     // POST: /campus
     @PostMapping
-    public ResponseEntity<Campus> addCampus(@RequestBody Campus campus) {
+    public ResponseEntity<CampusDto> addCampus(@RequestBody Campus campus) {
         Campus savedCampus = campusService.saveCampus(campus);
-        return new ResponseEntity<>(savedCampus, HttpStatus.CREATED);
+        return new ResponseEntity<>(CampusDto.from(savedCampus), HttpStatus.CREATED);
     }
 
     // EXCEPTIONS
